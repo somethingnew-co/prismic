f189d3edec4ab44fa01dc16c80c05b62d14d741d
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2020-07-27 19:52:36 -0400

    Add additional Prismic types

--- packages/prismic-types/index.ts
+++ packages/prismic-types/index.ts
@@ -1,3 +1,11 @@
+export interface LinkResolver {
+  (doc: PrismicDoc): string;
+}
+
+export interface Resolver {
+  (routes: {[ key: string] : string }, style?: string): LinkResolver;
+}
+
 export interface PrismicDoc {
   link_type: string;
   url: string;
@@ -19,10 +27,31 @@ export interface PrismicSlice {
   items: any[];
 }
 
-export interface LinkResolver {
-  (doc: PrismicDoc): string;
+export type PrismicDropdown = string
+
+export type PrismicKeyText = string
+
+export interface PrismicDocument {
+  [key: string]: any;
+  body: PrismicSlice[];
 }
 
-export interface Resolver {
-  (routes: {[ key: string] : string }, style?: string): LinkResolver;
+export interface PrismicLink {
+  link_type: string;
+  url: string;
+  id?: string;
+  type?: string;
+  tags?: string[];
+  slug?: string;
+  lang?: string;
+  uid?: string;
+  data?: any;
+  isBroken?: boolean;
+  target?: string;
+}
+
+export interface PrismicText {
+  type: string;
+  text: string;
+  spans: any[];
 }
