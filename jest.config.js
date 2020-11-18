4b09509a20a78bf98c47dabf1051a9c2d068a0d9
Cabe Branson <1258606+bitmap@users.noreply.github.com>
2020-11-18 18:12:34 -0500

    Add prismic-apollo-link package

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,7 @@ module.exports = {
   ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: [
+    'packages/.*/dist',
+  ],
 }
---
+++ packages/prismic-apollo-link/src/index.ts
@@ -0,0 +1,1 @@
+export { default as prismicApolloLink } from './apolloLink'
